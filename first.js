a5771e02acec94f4ce541279368bb3f7fbfb55f1
tolgahanbora <tolgahanbora@stu.aydin.edu.tr>
2022-10-17 18:42:35 +0300

    first commit

---
+++ first.js
@@ -0,0 +1,11 @@
+let r ;
+let triangle = (r) => {
+let pi = 3.14
+
+let total;
+return total = (pi * (r*r))
+ 
+}
+
+console.log(triangle())
+export default triangle
---
+++ index.js
@@ -0,0 +1,4 @@
+import triangle from "./first.js"; //çektiğin uzantının türün belirtmezsen çalışmıyor. javascript oldugunu "js" belirtmen gerek.
+
+
+triangle(12n)
